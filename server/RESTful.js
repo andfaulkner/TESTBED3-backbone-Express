44a4083f9ec3d8fb22f936defd0906bb9021fef7
andfaulkner <andfaulkner@gmail.com>
2015-09-11 02:50:44 -0400

    basic request-response from root api

--- server/RESTful.js
+++ server/RESTful.js
@@ -5,8 +5,27 @@ var log = require('server/winston-logger');
 //Build a new router object
 var restRouter = require('express').Router();
 
-restRouter.get('/', function(req, res){
-});
+restRouter
+    .get('/', function restRouter_base_get(req, res){
+        res.json({apiAccessed: true, protocol: "GET", msg: 'welcome to our api!'});
+        res.end();
+    })
+    .post('/', function restRouter_base_post(req, res){
+        res.json({apiAccessed: true, protocol: "POST", msg: 'welcome to our api!'});
+        res.end();
+    });
+
+restRouter
+    .post('/', function(req, res){
+        res.json({apiAccessed: true, protocol: "POST", msg: 'welcome to our api!'});
+        res.end();
+    });
+
+restRouter
+    .post('/', function(req, res){
+        res.json({apiAccessed: true, protocol: "POST", msg: 'welcome to our api!'});
+        res.end();
+    });
 
 //Output new router object, for use by the app.
 module.exports = restRouter;
