02efa0c74c3d244c125e572a105d63d42e524085
andfaulkner <andfaulkner@gmail.com>
2015-10-05 23:28:29 -0400

    added stuff

---
+++ client/components/tetris2/tetris2.js
@@ -0,0 +1,8 @@
+/* global $, _, React, async, Backbone, require */
+'use strict';
+
+(function tetrisJsFile(){
+    var tetrisIndex = require('./tetris2.jsx');
+
+    console.log(tetrisIndex);
+}());
